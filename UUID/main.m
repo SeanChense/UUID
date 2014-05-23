0ae2cecf5d0a903cc55de50dac55317630cdc598
sean <seanchense@wesharetech.com.cn>
2014-05-23 17:32:39 +0800

    done

--- UUID/main.m
+++ UUID/main.m
@@ -8,13 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create universally unique identifier (object)
+        CFUUIDRef uuidObject = CFUUIDCreate(kCFAllocatorDefault);
+        
+        // Get the string representation of CFUUID object.
+        NSString *uuidStr = (NSString *)CFBridgingRelease(CFUUIDCreateString(kCFAllocatorDefault, uuidObject)) ;
+        
+        // If needed, here is how to get a representation in bytes, returned as a structure
+        // typedef struct {
+        //   UInt8 byte0;
+        //   UInt8 byte1;
+        //   ...
+        //   UInt8 byte15;
+        // } CFUUIDBytes;
+        CFUUIDBytes bytes = CFUUIDGetUUIDBytes(uuidObject);
+        
+        CFRelease(uuidObject);
+        
+       NSLog(@"%@",uuidStr) ;
         
     }
     return 0;
