f05fa42496f23ba23a98ab4196997af993a000dd
sean <seanchense@wesharetech.com.cn>
2014-05-23 17:22:50 +0800

    Initial Commit

---
+++ UUID/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UUID
+//
+//  Created by 科技慧享 on 14-5-23.
+//  Copyright (c) 2014年 weshare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
